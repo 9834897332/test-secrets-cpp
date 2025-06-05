b2b73f0cdcd875a351b2b156d86162139ff8a86a
Kartik  Kohapare <87128102+9834897332@users.noreply.github.com>
2025-06-05 15:44:36 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
+// main.cpp
 #include <iostream>
 #include <cstdlib>
 
 int main() {
-    const char* secret = std::getenv("SECRET_CONFIG_FILE");
+    const char* secret = std::getenv("MY_SECRET_KEY");
     if (secret)
-        std::cout << "Secret (from env): " << secret << std::endl;
+        std::cout << "Runtime secret: " << secret << std::endl;
     else
-        std::cerr << "Secret not found in environment." << std::endl;
+        std::cerr << "Secret not found in env!" << std::endl;
     return 0;
 }
